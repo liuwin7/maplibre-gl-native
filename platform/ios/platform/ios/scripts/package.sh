ddc0d2dfd07cc43b60db011f2b42b9eef8280db1
liuwin7 <liuwin7@163.com>
2022-12-16 23:06:27 +0800

    改回Mapbox

--- platform/ios/platform/ios/scripts/package.sh
+++ platform/ios/platform/ios/scripts/package.sh
@@ -4,7 +4,7 @@ set -e
 set -o pipefail
 set -u
 
-NAME=UniMap
+NAME=Mapbox
 OUTPUT=build/ios/pkg
 DERIVED_DATA=build/ios
 PRODUCTS=${DERIVED_DATA}
@@ -111,7 +111,7 @@ if [[ ${BUILD_FOR_DEVICE} == true ]]; then
         -jobs ${JOBS} | tee ${LOG_PATH} | xcpretty
 fi
 
-LIBS=(UniMap.a)
+LIBS=(Mapbox.a)
 
 
 function copyAndMakeFatFramework {
